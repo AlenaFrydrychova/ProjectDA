d2bb5b72163304d41fcf1d175930a818c27b3f0d
Andrea Waltlova <andywaltlova@gmail.com>
2019-05-15 11:51:57 +0200

    no rating
    added script to scrape "no rating" text

--- Fajnsmekr_scrape_url.py
+++ Fajnsmekr_scrape_url.py
@@ -13,38 +13,38 @@ name_rest = rows_name[0].text #zatím jen název první restaurace
 
 #for row in rows: #sem potom bdue muset přijít všechno co se bdue dít na další otevřené stránce
 
-#celkové hodnocení restauracemi #zatim sice tiske hodnoty jednotlivých buněk, ale nevím jak je sčítat po řádkách a jak zajistit aby když nic nenajde byla 0
+#celkové hodnocení restaurací, vynechává nuly, což je trochu problém
 rest_rating = []
 poradi = 1 #pocita poradi radku, ale vynechava radky, ktere neobsahuji "td", {"class": "hbg1"}
 
-for line in table.find_all("td", {"class": "hbg1"}):
-    if line != 0:    
-        for rating in line.find_all("img"):
-            width = int(rating.get('width'))    
-            if width == 11:
-                value_rating += width
-                if width == 12:
-                    value_rating += width
-                    print(width)
-                    print(value_rating, poradi)
-                    value_rating = 0
+value_rating = 0
+for line in table.find_all("td", {"class": "hbg1"}): #hledá v tabulce tagy td s classou hbg1 (tam je definovaná délka obrázku, který představuje hodnocení)
+    if line != 0: #nefunguje, ideálně by se mělo spustit jen když v line tag td opravdu je a vypsat nulu, když není. Je třeba posunout podmínku nad cyklus a nějak jí přeformulovat?
+        for rating in line.find_all("img"): #najde všechny tagy img v td s classou hbg1
+            width = int(rating.get('width')) #hodnota width
+            if width == 11: # 11 je maximální délka jedné kostičky hodnocení (za předpokladu, že není na konci -> ta má z nějakého důvodu 12)
+                value_rating += width #hodnota do které se počítá rating jedné restaurace (celkový width obrázků)
+                if width == 12: #pokud má 12 jedná se o poslední kosticku hodnoceni
+                    value_rating += width #hodnota width
+                    print(poradi, value_rating) #poradí je jen císlo řádku, když jsme to hledaly v tabulce na webu
+                    value_rating = 0 #vymazání value rating, protože 12 má jen poslední kostička
                     poradi += 1
-            elif width > 0 and width < 11:
-                value_rating += width
-                print(width)
-                print(value_rating, poradi)
-                value_rating = 0
+            elif width > 0 and width < 11: # kostička, která je na konci a není kompletní tzn je mezi 1 až 10
+                value_rating += width #hodnota width
+                print(poradi, value_rating)
+                value_rating = 0 #vymazani hodnoty
                 poradi += 1
-    else:
+    else: #nefunguje
         value_rating = 0
         print(value_rating)
-  
+
 
  #poté bude v cyklu aby se název restaurace měnil
 """
-url_rest = "restaurace/cihelna1.aspx"
+url_rest = "restaurace/al_capone_pub.aspx"
 res1 = requests.get(url + url_rest)
 soup_restaurant = BeautifulSoup(res1.text, "lxml")
+
 #najde tabulku s classou popisrest a v ní vyhledá všechny tabulky a dá je do řádků (nevim jestli to vysvětluju správně), 10 až 15 řádek je info o restauraci, které chceme
 description_rest = soup_restaurant.find("table",  {"class": "popisrest"})
 output_rows = []
@@ -79,12 +79,18 @@ with open("info_rest.csv","w",encoding="utf-8",newline='') as csvfile:
 #Reviews text -> Issue s komentáři komentářů !
 rest_reviews = soup_restaurant.find('table', {'class': 'mytxt'})
 reviews_to_file = [] #seznam pro zápis textu reviews do souboru
-for review in rest_reviews.find_all('font', {'class': 'zobrazhodn'}):# Filtruje jen texty! :D
-    to_remove = review.find_all("div") #odstranila jsem tím komentář komentářů, ale nějakým záhadným způsobem tam je ještě jednou, přitom v html ho vidím jen jednou :D Div protože je to jediný div v tagu font
-    for element in to_remove:
-        element.extract()
-    text_reviews = name_rest + "#" + review.text
-    reviews_to_file.append(text_reviews) #seznam pro zápis do souboru
+no_rating = str(soup_restaurant.find('td', {'colspan': '7'}).text).strip()
+print(no_rating)
+if no_rating == "Nehodnoceno":
+    reviews_to_file.append("Nehodnoceno")
+    print(reviews_to_file)
+else:
+    for review in rest_reviews.find_all('font', {'class': 'zobrazhodn'}):# Filtruje jen texty! :D
+        to_remove = review.find_all("div") #odstranila jsem tím komentář komentářů, ale nějakým záhadným způsobem tam je ještě jednou, přitom v html ho vidím jen jednou :D Div protože je to jediný div v tagu font
+        for element in to_remove:
+            element.extract()
+        text_reviews = name_rest + "#" + review.text #zatím přidává jméno první restaurace, prootže nescrapujem všecky
+        reviews_to_file.append(text_reviews) #seznam pro zápis do souboru
 
 #zápis reviews do csv, pořád zapisuje i komentáře komentářů :(
 with open('reviews.csv', 'w',encoding='utf-8', newline='') as csvfile:
