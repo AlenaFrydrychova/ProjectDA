efc6bff39e28e084a12d349331b0364e32c9dd7d
Alena Frydrychova <alenecef@gmail.com>
2019-05-20 22:33:07 +0200

    fixed issue with sentiment analysis

--- sentiment.py
+++ sentiment.py
@@ -2,25 +2,19 @@ import requests
 import csv
 import xlrd
 
+
+#FUKNCE NA ZJISTENI SENTIMENTU - POSLE REQUEST NA GENEEA.COM
 def get_sentiment(chunk):
     payload = {
         'text': chunk,
-        'user_key': 'b0273bf8e53597cc65ddaab58d6ad530'
+        'user_key': 'f61bdd3e6f1024513115068d79c6009d'
     }
     r = requests.get('https://api.geneea.com/s2/sentiment', params=payload)
     return(r.json())
+    print(r.json())
 
 
-def analyzuj_recenzi(reviews, recenze):
-	hodnoty = []
-
-	for recenze in reviews[:10]:
-		try:
-			hodnoty.append(get_sentiment(recenze)['sentiment'])
-		except KeyError:
-			pass
-	return(hodnoty)
-
+#OTEVIRANI SOUBORU POMOCI KNIHOVNY XLRD, ROZDELENI OBSAHU PODLE RADKU
 workbook = xlrd.open_workbook('C:\\Users\\Alena\\Documents\\Da Czechitas\\projekt\\ProjectDA\\reviews.xlsx', 'rb')
 sheet = workbook.sheet_by_index(0)
 rows = []
@@ -30,19 +24,21 @@ for i in range(sheet.nrows):
         columns.append(sheet.cell(i, j).value)
     rows.append(columns)
 
-
+# VYTAHNE Z RADKU 3. HODNOTU - REVIEW
 reviews = []
 for i in rows[1:]:
     reviews.append(i[2])
+   
 
-
+#POUZIJE FUNKCI ANALYZUJ_RECENZI A HODNOTY ULOZI DO LISTU
 hodnoty_tofile = []
 for recenze in reviews:
-    print(recenze, analyzuj_recenzi(reviews, recenze))
+        try:
+            hodnoty_tofile.append(get_sentiment(recenze)['sentiment'])
+        except KeyError:
+            hodnoty_tofile.append('error')
 
 
-
-with open('sentiment.csv', 'w', encoding='utf-8') as csvfile:
-	writer = csv.writer(csvfile)
-	for value in hodnoty_tofile:
-		writer.writerow([value])
+with open('reviews_analysis.csv', 'w', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(zip(reviews, hodnoty_tofile))
