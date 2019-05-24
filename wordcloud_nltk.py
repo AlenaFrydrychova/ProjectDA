231c462a6961d1c49def580d756a7e6952df1192
Andrea Waltlova <andywaltlova@gmail.com>
2019-05-24 22:05:37 +0200

    word frequency as pandas dataframe

--- wordcloud_nltk.py
+++ wordcloud_nltk.py
@@ -29,7 +29,7 @@ prepositions = ["od","z","s","do","bez","krom","kromě","podle","okolo","vedle",
 conjunctions = ["a", "i", "ani", "nebo", "či", "přímo", "nadto", "ani", "jak", "tak", "hned", "jednak", "zčásti", "dílem", "ale", "avšak", "však", "leč", "nýbrž", "naopak", "jenomže", "jenže", "sice", "jistě", "ale", "i", "ba", "ba i", "ba ani", "nadto", "dokonce", "nejen", "nebo", "anebo", "buď", "totiž", "vždyť", "neboť", "vždyť", "totiž", "však", "také", "proto", "a proto", "a tak", "tudíž", "a tudíž", "tedy"]
 pronouns = ["já", "ty", "on", "ona", "ono", "my", "vy", "oni", "ony", "ona","se", "můj", "tvůj", "jeho", "její", "náš", "váš", "svůj", "ten", "tento", "tenhle", "onen", "takový", "týž", "tentýž", "sám", "kdo", "co", "jaký", "který","čí", "jenž", "nikdo", "nic", "nijaký", "ničí", "žádný", "někdo", "nějaký", "některý", "lecco", "něčí", "něco"]
 verbs = ["být","bejt","jsem","jsi","je","jest","jsme","jste","jsou","budu","budeš","bude","budeme","budete","budou","buď","budiž","buďme","buďmež","buďte","buďtež","byl","byla","bylo","byli","byly","jsa","jsouc","jsouce","byv","byvše","byvši","bych","bychom","bys","byste","by", "seš", "mám","máš","má","máme","máte","mají","měj","mějme","mějte","měl","měla","mělo","měli","měly","maje","majíc","majíce"]
-another_specific = ["si","jen","mi","mě","tady","tomu","že","to","nám","ná","takže","jako","už","pokud","asi","celkem","docela","tam","dali","ze","ve","ji","ta","pak","taky","což","tím","již","možná","která","toho","protože","sem","kde","která","které","tu","než","když","Kč","při","až","ho","této","mne","aby","nebyl","tuto","tom","No","kdy","dal","nebyla","jejich","jinak","zde","kterou","toto","dala","ní","nás","mu","dostali","objednali","jím","myslím","jim","Já","Jen","námi","Na","Po",")","(",".",",",":","!","...","-","?"]
+another_specific = ["si","jen","mi","mě","tady","tomu","že","to","nám","ná","takže","jako","už","pokud","asi","celkem","docela","tam","dali","ze","ve","ji","ta","pak","taky","což","tím","již","možná","která","toho","protože","sem","kde","která","které","tu","než","když","Kč","při","až","ho","této","mne","aby","nebyl","tuto","tom","No","kdy","dal","nebyla","jejich","jinak","zde","kterou","toto","dala","ní","nás","mu","dostali","objednali","jím","myslím","jim","Já","Jen","námi","Na","Po","není",")","(",".",",",":","!","...","-","?"]
 
 #STOPWORDS WITHOUT DIACRITICS LOWERCASE
 prepositions_without = [''.join((c for c in unicodedata.normalize('NFD', preposition) if unicodedata.category(c) != 'Mn')) for preposition in prepositions]
@@ -57,7 +57,7 @@ reviews_dict = {"review" : reviews} #z lsitu slovník, aby měl sloupec header (
 df = pd.DataFrame(reviews_dict) #pandas dataframe ze slovníku reviews
 text = " ".join(review for review in df.review) #recenze
 
-wordcloud = WordCloud(width=800, height=400, stopwords=stopwords, max_words=120, background_color="white").generate(text)
+wordcloud = WordCloud(width=800, height=400, stopwords=stopwords_cz, max_words=120, background_color="white").generate(text)
 
 plt.figure(figsize=(15,10))
 plt.imshow(wordcloud, interpolation='bilinear')
@@ -81,19 +81,22 @@ tokens = [token for token in tokens if not token in stopwords]
 clean_reviews = " ".join(tokens)
 
 """
+#Zobrazení shody ukazuje každý výskyt daného slova spolu s určitým kontextem
 t = nltk.WhitespaceTokenizer()
 textList = Text(t.tokenize(string_reviews))
 sys.stdout = open('concordance.txt', 'w', encoding="utf-8")#zápis do souboru (přesměruje výsledek printu do souboru)
 textList.concordance("obsluha", lines=100)
 sys.stdout = sys.__stdout__ #vrátí výsledky printu do příkazové řádky
 
+#lexikální bohatost textu neboli počet odlišných slov v textu
 richness = (len(set(textList)) / len(textList))*100
-print(round(richness,2), "%") #lexikální bohatost textu neboli počet odlišných slov v textu
+print(round(richness,2), "%")
 """
-
+#FREKVENCE SLOV (potřeba lemmatizovat alespoň některá a zapsat je do nějaké tabulky, ideální by bylo udělat zvlášť frekvenci slov pro pozitivní a negativní recenze, stejně tak wordcloud, který je momentálně ze všech recenzí)
 fdist = FreqDist()
 for word in word_tokenize(clean_reviews):
     fdist[word.lower()] += 1
 words = fdist.most_common(15)
-for word in words:
-    print(word)
+
+df = pd.DataFrame(words ,columns=["word","count"])
+print(df)
